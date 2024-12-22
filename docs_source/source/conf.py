27996f6ddaea87da4c5d83d000f479a6a5e2348f
Nelson Gougou <nelsongougou@MacBook-Pro-de-Nelson.local>
2024-12-22 11:34:22 +0000

    reformat: black .

--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -6,7 +6,8 @@
 # -- Path setup -----------------------------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../starburst_api'))
+
+sys.path.insert(0, os.path.abspath("../../starburst_api"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
