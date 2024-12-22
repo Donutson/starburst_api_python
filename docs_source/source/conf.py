f30f2d4ec6c22614743721c8940ec0e26b709b4e
Nelson Gougou <nelsongougou@MacBook-Pro-de-Nelson.local>
2024-12-22 05:34:37 +0000

    docs: configure autodoc

--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -33,3 +33,7 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# -- Options for autodoc ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+autoclass_content = "both"
