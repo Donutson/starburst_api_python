11b04252900586d184fd13a98f051eaf464ef7c7
Nelson Gougou <nelsongougou@MacBook-Pro-de-Nelson.local>
2024-12-22 04:54:33 +0000

    docs: add new extensions

--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -14,7 +14,8 @@ release = '0.5.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.githubpages',
+    'sphinx.ext.autodoc',]
 
 templates_path = ['_templates']
 exclude_patterns = []
